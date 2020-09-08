5acdbd5950417f6de264c94a69727cca4947a96f
lorenzo-damico <loredam@hotmail.it>
2020-09-08 20:44:07 +0200

    Aggiunto inserimento testo con input

--- js/script.js
+++ js/script.js
@@ -1,3 +1,35 @@
 $(document).ready(function () {
 
+  // Funzione che scrive il messaggio
+  function writeMessage() {
+    var inputValue = $("#writer").val();
+
+    if (inputValue != "") {
+      var elemento = $(".template-send > div").clone();
+      elemento.find("p").text(inputValue);
+      console.log(elemento);
+      $(".chat-screen-section").append(elemento);
+      $("#writer").val("");
+    }
+  }
+
+
+  $("#writer").keyup(
+    function (event) {
+      if (event.which == 13) {
+        writeMessage();
+      }
+    }
+  );
+
+  $(".fa-paper-plane").click(
+    function () {
+      writeMessage();
+    }
+  );
+
+
+
+
+
 });
