42d4d2a6babb3c98a625b56048f25f4a2ca876c9
lorenzo-damico <loredam@hotmail.it>
2020-09-08 21:50:21 +0200

    Aggiustamenti

--- js/script.js
+++ js/script.js
@@ -1,19 +1,18 @@
 $(document).ready(function () {
 
-  // Funzione che scrive il messaggio
+  // Funzione che scrive il messaggio.
   function writeMessage() {
     var inputValue = $("#writer").val();
 
     if (inputValue != "") {
       var elemento = $(".template-send > div").clone();
       elemento.find("p").text(inputValue);
-      console.log(elemento);
       $(".chat-screen-section").append(elemento);
       $("#writer").val("");
     }
   }
 
-
+  // Stampo alla pressione del tasto.
   $("#writer").keyup(
     function (event) {
       if (event.which == 13) {
@@ -21,7 +20,8 @@ $(document).ready(function () {
       }
     }
   );
-
+  
+  // Stampo al click sul pulsante.
   $(".fa-paper-plane").click(
     function () {
       writeMessage();
