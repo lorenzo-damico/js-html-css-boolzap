e8ce9cc2c63b2d0351e6b694d5b7b1fa3b55e870
lorenzo-damico <loredam@hotmail.it>
2020-09-09 13:15:34 +0200

    Aggiunta risposta dopo 1 secondo

--- js/script.js
+++ js/script.js
@@ -1,30 +1,70 @@
 $(document).ready(function () {
 
+  // Funzione che acquisisce l'orario.
+  function currentTime() {
+    var date = new Date();
+    var hours = date.getHours();
+    var minutes = date.getMinutes();
+    if (minutes > 9) {
+      var time = hours + ":" + minutes;
+    } else {
+      var time = hours + ":0" + minutes;
+    }
+    return time;
+  }
+
   // Funzione che scrive il messaggio.
   function writeMessage() {
-    var inputValue = $("#writer").val();
 
-    if (inputValue != "") {
-      var elemento = $(".template-send > div").clone();
-      elemento.find("p").text(inputValue);
-      $(".chat-screen-section").append(elemento);
-      $("#writer").val("");
-    }
+    var inputValue = $("#writer").val();
+    var elemento = $(".template-send .box-message-container").clone();
+    var time = currentTime();
+    elemento.find("p").text(inputValue);
+    elemento.find(".message-box-time").text(time);
+    $(".chat-screen-section").append(elemento);
+    $("#writer").val("");
   }
+
+  // Funzione che risponde al messaggio.
+  function answerMessage(answerValue) {
+
+    var elemento = $(".template-received .box-message-container").clone();
+    var time = currentTime();
+    elemento.find("p").text(answerValue);
+    elemento.find(".message-box-time").text(time);
+    $(".chat-screen-section").append(elemento);
+  }
+
 
   // Stampo alla pressione del tasto.
   $("#writer").keyup(
     function (event) {
       if (event.which == 13) {
-        writeMessage();
+        var inputValue = $("#writer").val();
+        if (inputValue != "") {
+          writeMessage();
+          setTimeout(
+            function () {
+              answerMessage("OK");
+            }, 1000
+          );
+        }
       }
     }
   );
-  
+
   // Stampo al click sul pulsante.
   $(".fa-paper-plane").click(
     function () {
-      writeMessage();
+      var inputValue = $("#writer").val();
+      if (inputValue != "") {
+        writeMessage();
+        setTimeout(
+          function () {
+            answerMessage("OK");
+          }, 1000
+        );
+      }
     }
   );
 
